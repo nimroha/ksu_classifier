e811458df6b89f5306e1ad699c415a5994f37208
Yuval Nissan <yuval.nissan4@gmail.com>
2018-06-01 18:15:33 +0300

    updated mnistRunner.py

--- DataSets/mnistRunner.py
+++ DataSets/mnistRunner.py
@@ -1,36 +1,32 @@
 import sys
-from mnist import MNIST
+import mnist
 from sklearn.utils import shuffle
 import numpy as np
-import logging
 from time import time
 
 from ksu.KSU import KSU
 
 def main(argv=None):
 
-    mnist_dir = 'C:\Users\yuvalnissan\Desktop\MNIST'
-    mndata = MNIST(mnist_dir)
-    mndata.gz = True
-    mndata.load_training()
-    mndata.load_testing()
+    trainImages, trainLabels = shuffle(mnist.train_images(), mnist.train_labels())
+    testImages, testLabels = shuffle(mnist.test_images(), mnist.test_labels())
 
-    train, trainLabels = shuffle(mndata.train_images, mndata.train_labels)
-    test, testLabels = shuffle(mndata.test_images, mndata.test_labels)
+    train_n = int(0.1 * len(trainLabels))
+    test_n = int(0.1 * len(testLabels))
 
-    train = np.array(train[:9999])
-    trainLabels = np.array(trainLabels[:9999])
-    test = np.array(test[:999])
-    testLabels = np.array(testLabels[:999])
+    trainImages = np.reshape(np.array(trainImages[0:train_n,:,:]), [train_n, -1])
+    trainLabels = np.array(trainLabels[0:train_n])
+    testImages = np.reshape(np.array(testImages[0:test_n,:,:]), [test_n, -1])
+    testLabels = np.array(testLabels[0:test_n])
 
     start = time()
-    ksu = KSU(train, trainLabels, 'l2')
+    ksu = KSU(trainImages, trainLabels, 'l2')
     end = time()
 
     print("Train time: %d", end - start)
 
     ksuClassifier = ksu.makePredictor(0.1)
-    predictedLabels = ksuClassifier.predict(test)
+    predictedLabels = ksuClassifier.predict(testImages)
     error = np.mean(predictedLabels != testLabels)
 
     print(error)
